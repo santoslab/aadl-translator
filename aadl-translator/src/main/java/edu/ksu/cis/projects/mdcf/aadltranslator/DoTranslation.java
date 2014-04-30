fe1318fe43190182e280aa0207d74e89464e51ea
Sam Procter <samprocter@k-state.edu>
2014-04-30 11:22:14 -0500

    Minor refactor for readability

--- aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/DoTranslation.java
+++ aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/DoTranslation.java
@@ -63,6 +63,39 @@ public final class DoTranslation implements IHandler, IRunnableWithProgress {
 				new NullProgressMonitor());
 
 		// 1) Get the current selection, convert it to an IFile
+		IFile file = getIFileFromSelection();
+
+		// 2) Get the target element (hopefully a system)
+		Element target = getTargetElement(file);
+
+		// 3) Verify that the target is a system. If it isn't, abort the
+		// translation
+		if (!isSystem(target))
+			return null;
+
+		// 4) Recursively traverse includes to build up IFile list
+		ic.process(target);
+
+		// 5) Return the used files.
+		return ic.getUsedFiles();
+	}
+
+	private boolean isSystem(Element target) {
+		AadlPackage pack = (AadlPackage) target;
+		PublicPackageSection sect = pack.getPublicSection();
+		Classifier ownedClassifier = sect.getOwnedClassifiers().get(0);
+		return ownedClassifier instanceof org.osate.aadl2.System;
+	}
+
+	private Element getTargetElement(IFile file) {
+		ResourceSet rs = OsateResourceUtil.createResourceSet();
+		Resource res = rs.getResource(
+				OsateResourceUtil.getResourceURI((IResource) file), true);
+		Element target = (Element) res.getContents().get(0);
+		return target;
+	}
+
+	private IFile getIFileFromSelection() {
 		ISelection selection = HandlerUtil.getCurrentSelection(triggeringEvent);
 		IFile file = null;
 		if (selection instanceof TextSelection) {
@@ -73,44 +106,164 @@ public final class DoTranslation implements IHandler, IRunnableWithProgress {
 			file = (IFile) ((IAdaptable) ((IStructuredSelection) selection)
 					.getFirstElement()).getAdapter(IFile.class);
 		}
-
-		// 2) Verify that the selection contains a system. If it does: continue;
-		// if not: abort the translation
-		ResourceSet rs = OsateResourceUtil.createResourceSet();
-		Resource res = rs.getResource(
-				OsateResourceUtil.getResourceURI((IResource) file), true);
-		Element target = (Element) res.getContents().get(0);
-		AadlPackage pack = (AadlPackage) target;
-		PublicPackageSection sect = pack.getPublicSection();
-		Classifier ownedClassifier = sect.getOwnedClassifiers().get(0);
-		if (!(ownedClassifier instanceof org.osate.aadl2.System))
-			return null;
-
-		// 3) Recursively traverse includes to build up IFile list
-		ic.process(target);
-
-		return ic.getUsedFiles();
+		return file;
 	}
 
 	public void doTranslation(IProgressMonitor monitor) {
+		// 1) Initialize the progress monitor and translator
+		ResourceSet rs = initProgressMonitor(monitor);
+		Translator stats = new Translator(monitor);
+		
+		// 2) Get the list of files used in the model we're translating
+		HashSet<IFile> usedFiles = this.getUsedFiles();
 
+		// 3) Identify which file contains the AADL system
+		IFile systemFile = getSystemFile(rs, stats, usedFiles);
+		
+		// 4) Initialize the error reporter
+		ParseErrorReporterManager parseErrManager = initErrManager(stats);
+		
+		// 5) Build the in-memory system model
+		processFiles(monitor, rs, stats, usedFiles, systemFile, parseErrManager);
+		
+		// 6) Write the generated files
+		writeOutput(stats);
+
+		// 7) Shut down the progress monitor
+		wrapUpProgressMonitor(monitor);
+	}
+
+	private void wrapUpProgressMonitor(IProgressMonitor monitor) {
+		monitor.worked(1);
+		monitor.done();
+	}
+
+	private ResourceSet initProgressMonitor(IProgressMonitor monitor) {
 		OsateResourceUtil.refreshResourceSet();
-
 		ResourceSet rs = OsateResourceUtil.createResourceSet();
 		HashSet<IFile> files = TraverseWorkspace
 				.getAadlandInstanceFilesInWorkspace();
 
 		monitor.beginTask("Translating AADL to Java / MIDAS", files.size() + 1);
+		return rs;
+	}
 
+	private void writeOutput(Translator stats) {
+		// Filename -> file contents
+		HashMap<String, String> compsigs = new HashMap<>();
+
+		// Filename -> file contents
+		HashMap<String, String> javaClasses = new HashMap<>();
+
+		String appName;
+		String appSpecContents;
+		IPreferencesService service = Platform.getPreferencesService();
+		
+		// Get user preferences
+		boolean generateShells = service.getBoolean(
+				"edu.ksu.cis.projects.mdcf.aadl-translator",
+				PreferenceConstants.P_USERSHELLS, true, null);
+		String appDevDirectory = service.getString(
+				"edu.ksu.cis.projects.mdcf.aadl-translator",
+				PreferenceConstants.P_APPDEVPATH, null, null);
+
+		// Configure stringtemplate delimeters
+		midas_compsigSTG.delimiterStartChar = '$';
+		midas_compsigSTG.delimiterStopChar = '$';
+		midas_appspecSTG.delimiterStartChar = '#';
+		midas_appspecSTG.delimiterStopChar = '#';
+		
+		// Give the model to the string templates
+		for (ProcessModel pm : stats.getSystemModel().getLogicComponents()
+				.values()) {
+			javaClasses.put(pm.getName() + "SuperType", java_superclassSTG
+					.getInstanceOf("class").add("model", pm).render());
+			if (generateShells) {
+				javaClasses.put(
+						pm.getName(),
+						java_userimplSTG.getInstanceOf("userimpl")
+								.add("model", pm).render());
+			}
+			compsigs.put(pm.getName(), midas_compsigSTG
+					.getInstanceOf("compsig").add("model", pm).render());
+		}
+
+		appName = stats.getSystemModel().getName();
+		appSpecContents = midas_appspecSTG.getInstanceOf("appspec")
+				.add("system", stats.getSystemModel()).render();
+
+		// Write the files
+		if (stats.notCancelled()) {
+			WriteOutputFiles.writeFiles(compsigs, javaClasses, appName,
+					appSpecContents, appDevDirectory);
+		}
+	}
+
+	private void processFiles(IProgressMonitor monitor, ResourceSet rs,
+			Translator stats, HashSet<IFile> usedFiles, IFile systemFile,
+			ParseErrorReporterManager parseErrManager) {
+
+		// Process the system first...
+		processFile(monitor, rs, stats, systemFile, parseErrManager);
+
+		// Now process all the other files
+		for (IFile f : usedFiles) {
+			processFile(monitor, rs, stats, f, parseErrManager);
+		}
+	}
+
+	private void processFile(IProgressMonitor monitor, ResourceSet rs,
+			Translator stats, IFile systemFile,
+			ParseErrorReporterManager parseErrManager) {
+		Resource res = rs.getResource(
+				OsateResourceUtil.getResourceURI((IResource) systemFile), true);
+
+		// Delete any existing error markers in the system file
+		IResource file = OsateResourceUtil.convertToIResource(res);
+		ParseErrorReporter errReporter = parseErrManager.getReporter(file);
+		((MarkerParseErrorReporter) errReporter).setContextResource(res);
+		errReporter.deleteMessages();
+
+		Element target = (Element) res.getContents().get(0);
+		stats.process(target);
+		monitor.worked(1);
+	}
+
+	private ParseErrorReporterManager initErrManager(Translator stats) {
+		// The ParseErrorReporter provided by the OSATE model support is nearly
+		// perfect here, we only change the marker id (much of the code is
+		// directly lifted from elsewhere in the OSATE codebase
+		ParseErrorReporterFactory parseErrorLoggerFactory = new LogParseErrorReporter.Factory(
+				OsateCorePlugin.getDefault().getBundle());
+		ParseErrorReporterManager parseErrManager = new ParseErrorReporterManager(
+				new MarkerParseErrorReporter.Factory(
+						"edu.ksu.cis.projects.mdcf.aadl-translator.TranslatorErrorMarker",
+						parseErrorLoggerFactory));
+
+		stats.setErrorManager(parseErrManager);
+		return parseErrManager;
+	}
+
+	/**
+	 * This method does two things. One, it returns the (first) file containing
+	 * an AADL system, and two, it removes that file from the set of usedFiles.
+	 * 
+	 * @param rs
+	 *            The set of resources in the workspace
+	 * @param stats
+	 *            The translator implementation
+	 * @param usedFiles
+	 *            The set of files used in the architecture description. Note
+	 *            that this parameter is modified (to not include the AADL file
+	 *            containing the system) by this method.
+	 * @return The file containing the AADL system.
+	 */
+	private IFile getSystemFile(ResourceSet rs, Translator stats,
+			HashSet<IFile> usedFiles) {
 		IFile systemFile = null;
-
-		Translator stats = new Translator(monitor);
-		HashSet<IFile> usedFiles = this.getUsedFiles();
-		// The system _has_ to come first, so we make sure it's first
 		for (IFile f : usedFiles) {
 			Resource res = rs.getResource(
 					OsateResourceUtil.getResourceURI((IResource) f), true);
-
 			Element target = (Element) res.getContents().get(0);
 			if ((target instanceof PropertySet)) {
 				String propertySetName = ((PropertySet) target).getName();
@@ -129,98 +282,7 @@ public final class DoTranslation implements IHandler, IRunnableWithProgress {
 				}
 			}
 		}
-
-		// The ParseErrorReporter provided by the OSATE model support is nearly
-		// perfect here, we only change the marker id (much of the code is
-		// directly lifted from elsewhere in the OSATE codebase
-		final ParseErrorReporterFactory parseErrorLoggerFactory = new LogParseErrorReporter.Factory(
-				OsateCorePlugin.getDefault().getBundle());
-		final ParseErrorReporterManager parseErrManager = new ParseErrorReporterManager(
-				new MarkerParseErrorReporter.Factory(
-						"edu.ksu.cis.projects.mdcf.aadl-translator.TranslatorErrorMarker",
-						// "aadl-translator.TranslatorErrorMarker",
-						parseErrorLoggerFactory));
-
-		stats.setErrorManager(parseErrManager);
-
-		// Process the system first...
-		Resource res = rs.getResource(
-				OsateResourceUtil.getResourceURI((IResource) systemFile), true);
-
-		// Delete any existing error markers in the system file
-		IResource file = OsateResourceUtil.convertToIResource(res);
-		ParseErrorReporter errReporter = parseErrManager.getReporter(file);
-		((MarkerParseErrorReporter) errReporter).setContextResource(res);
-		errReporter.deleteMessages();
-
-		Element target = (Element) res.getContents().get(0);
-		stats.process(target);
-		monitor.worked(1);
-
-		// Now process all the other files
-		for (IFile f : usedFiles) {
-			res = rs.getResource(
-					OsateResourceUtil.getResourceURI((IResource) f), true);
-
-			// Delete any existing error markers in this file
-			file = OsateResourceUtil.convertToIResource(res);
-			errReporter = parseErrManager.getReporter(file);
-			((MarkerParseErrorReporter) errReporter).setContextResource(res);
-			errReporter.deleteMessages();
-
-			target = (Element) res.getContents().get(0);
-			stats.process(target);
-			monitor.worked(1);
-		}
-
-		// Filename -> file contents
-		HashMap<String, String> compsigs = new HashMap<>();
-
-		// Filename -> file contents
-		HashMap<String, String> javaClasses = new HashMap<>();
-
-		String appName;
-		String appSpecContents;
-		IPreferencesService service = Platform.getPreferencesService();
-		boolean generateShells = service.getBoolean(
-				"edu.ksu.cis.projects.mdcf.aadl-translator",
-				// "aadl-translator",
-				PreferenceConstants.P_USERSHELLS, true, null);
-		String appDevDirectory = service.getString(
-				"edu.ksu.cis.projects.mdcf.aadl-translator",
-				// "aadl-translator",
-				PreferenceConstants.P_APPDEVPATH, null, null);
-
-		midas_compsigSTG.delimiterStartChar = '$';
-		midas_compsigSTG.delimiterStopChar = '$';
-		for (ProcessModel pm : stats.getSystemModel().getLogicComponents()
-				.values()) {
-			javaClasses.put(pm.getName() + "SuperType", java_superclassSTG
-					.getInstanceOf("class").add("model", pm).render());
-			if (generateShells) {
-				javaClasses.put(
-						pm.getName(),
-						java_userimplSTG.getInstanceOf("userimpl")
-								.add("model", pm).render());
-			}
-			compsigs.put(pm.getName(), midas_compsigSTG
-					.getInstanceOf("compsig").add("model", pm).render());
-		}
-		midas_appspecSTG.delimiterStartChar = '#';
-		midas_appspecSTG.delimiterStopChar = '#';
-
-		appName = stats.getSystemModel().getName();
-		appSpecContents = midas_appspecSTG.getInstanceOf("appspec")
-				.add("system", stats.getSystemModel()).render();
-
-		if (stats.notCancelled()) {
-			WriteOutputFiles.writeFiles(compsigs, javaClasses, appName,
-					appSpecContents, appDevDirectory);
-		}
-
-		monitor.worked(1);
-
-		monitor.done();
+		return systemFile;
 	}
 
 	@Override
