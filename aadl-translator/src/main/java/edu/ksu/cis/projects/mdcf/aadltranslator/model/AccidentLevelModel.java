e6b7c7e12b73e8e597575b1795661215ffc63aa3
Sam Procter <sam.procter@gmail.com>
2015-01-28 15:30:11 -0500

    Bring breaking changes back in now that we're on develop

--- aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/model/AccidentLevelModel.java
+++ aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/model/AccidentLevelModel.java
@@ -1,5 +1,14 @@
 package edu.ksu.cis.projects.mdcf.aadltranslator.model;
 
 public class AccidentLevelModel extends StpaPreliminaryModel {
+	protected int number;
 
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
--- aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/model/StpaPreliminaryModel.java
+++ aadl-translator/src/main/java/edu/ksu/cis/projects/mdcf/aadltranslator/model/StpaPreliminaryModel.java
@@ -2,7 +2,6 @@ package edu.ksu.cis.projects.mdcf.aadltranslator.model;
 
 public class StpaPreliminaryModel {
 	protected String name;
-	protected int number;
 	protected String description;
 	protected StpaPreliminaryModel parent;
 	
@@ -12,14 +11,6 @@ public class StpaPreliminaryModel {
 	
 	public void setName(String name) {
 		this.name = name;
-	}
-	
-	public int getNumber() {
-		return number;
-	}
-	
-	public void setNumber(int number) {
-		this.number = number;
 	}
 	
 	public String getDescription() {
